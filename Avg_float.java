eb879075bd474faa73cc9e6408cfd1892f0c091c
Shubranshu07 <goutamnayak691@gmail.com>
2025-07-26 12:08:57 +0530

    new

---
+++ Avg_float.java
@@ -0,0 +1,13 @@
+
+public class Avg_float
+{
+    public void main(float First,float Second, float Third, float Fourth ,float Fifth)
+    {
+      float avg = First+Second+Third+Fourth+Fifth/5;
+      
+      if(avg>50)
+      System.out.println(" GOOD ! ");
+      else
+      System.out.println(" BAD ! ");
+    }
+}
