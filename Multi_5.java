e0ba2a0c6246c5a7acc05ee07442017b84d99a4a
Shubranshu07 <goutamnayak691@gmail.com>
2025-07-27 09:18:45 +0530

    g

---
+++ Multi_5.java
@@ -0,0 +1,11 @@
+
+public class Multi_5
+{
+    void main(int n)
+    {
+        if(n%5==0)
+        System.out.println("The number " + n + " is a multiple of 5 ");
+        else 
+        System.out.println("The number is " + n + " is not a mulltiple of 5 ");
+    }
+}
